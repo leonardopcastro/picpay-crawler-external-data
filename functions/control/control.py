c20a697f26a6853da895243846bb2839fc9ff6c4
leonardopcastro <leonardo.castro@picpay.com>
2021-09-21 13:26:36 -0300

    Novos parâmetros control
    adicionando data atualização update_at e frequência de atualização update_frequency ao control

--- functions/control/control.py
+++ functions/control/control.py
@@ -1,4 +1,3 @@
-import copy
 import yaml
 from datetime import datetime
 
@@ -20,17 +19,26 @@ def control(event: dict, context: dict) -> list:
     # Pega os metadados no s3
     metadata = get_file_from_s3(Config.BUCKET_NAME, f'{Config.BUCKET_METADATA_KEY}/metadata.yaml')
 
-    # # Atualiza a data inicial de cada parametro
-    cp_data = copy.deepcopy(metadata)
-    for item in cp_data:
-        if 'start_date' in item['params']:
-            item['params']['start_date'] = datetime.now().strftime('%Y-%m-%d')
+    # Seleciona quais bases vão ser extraidas e atualiza registros
+    data = []
+    date_now = datetime.date(datetime.now())
+    for d in metadata:
+        if not d.get('update_at'):
+            data.append(d)
+            d['update_at'] = date_now
+        else:
+            if (d['update_frequency'] == 'year' and d['update_at'].year != date_now.year) or \
+                    (d['update_frequency'] == 'month' and d['update_at'].month != date_now.month) or \
+                    (d['update_frequency'] == 'day' and d['update_at'].day != date_now.day):
+                data.append(d)
+                d['update_at'] = date_now
 
     # Salva nova versão no s3
-    save_file_into_s3(yaml.dump(metadata), Config.BUCKET_NAME, f'{Config.BUCKET_METADATA_KEY}/metadata.yaml')
+    if data:
+        save_file_into_s3(yaml.dump(metadata), Config.BUCKET_NAME, f'{Config.BUCKET_METADATA_KEY}/metadata.yaml')
 
     # Retorno dos metadados das extrações
-    return metadata
+    return data
 
 if __name__ == '__main__':
     control({}, None)
