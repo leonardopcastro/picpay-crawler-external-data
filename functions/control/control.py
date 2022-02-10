b44125d8cd03c9465f4ac02a7eeda25d9ecf9799
leonardopcastro <leonardo.castro@picpay.com>
2022-02-10 16:29:38 -0300

    Correções control e metadados

--- functions/control/control.py
+++ functions/control/control.py
@@ -1,7 +1,4 @@
-import yaml
-from datetime import datetime
-
-from aws.aws_layer import get_file_from_s3, save_file_into_s3
+from aws.aws_layer import get_file_from_s3
 from config.config import Config
 
 
@@ -19,23 +16,8 @@ def control(event: dict, context: dict) -> list:
     # Pega os metadados no s3
     metadata = get_file_from_s3(Config.BUCKET_NAME, f'{Config.BUCKET_METADATA_KEY}/metadata.yaml')
 
-    # Seleciona quais bases vão ser extraidas e atualiza registros
-    data = []
-    date_now = datetime.date(datetime.now())
-    for d in metadata:
-        if not d.get('update_at'):
-            data.append(d)
-            d['update_at'] = date_now.strftime('%Y-%m-%d')
-        else:
-            if (d['update_frequency'] == 'year' and d['update_at'].year != date_now.year) or \
-                    (d['update_frequency'] == 'month' and d['update_at'].month != date_now.month) or \
-                    (d['update_frequency'] == 'day' and d['update_at'].day != date_now.day):
-                data.append(d)
-                d['update_at'] = date_now.strftime('%Y-%m-%d')
-
-    # Salva nova versão no s3
-    if data:
-        save_file_into_s3(yaml.dump(metadata), Config.BUCKET_NAME, f'{Config.BUCKET_METADATA_KEY}/metadata.yaml')
+    # Seleciona quais bases vão ser extraidas
+    data = [d for d in metadata if d.get('is_active')]
 
     # Retorno dos metadados das extrações
     return data
